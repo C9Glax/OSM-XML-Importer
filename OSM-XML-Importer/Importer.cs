ef1593045a82a0ebfc7272dc829607a50eedacf7
C9Glax <13404778+C9Glax@users.noreply.github.com>
2022-11-18 13:49:52 +0100

    Added from https://github.com/C9Glax/OSM-Landmarks https://github.com/C9Glax/OSM-Landmarks/commit/352d8746babd55b6980ee452ac893c1e744834ee

--- OSM-XML-Importer/Importer.cs
+++ OSM-XML-Importer/Importer.cs
@@ -1,9 +1,8 @@
-﻿#pragma warning disable CS8600
-#pragma warning disable CS8602
-#pragma warning disable CS8604
+﻿#pragma warning disable CS8600, CS8601, CS8602, CS8604 //All Attributes have to be present
 using Logging;
 using System.Xml;
 using GeoGraph;
+using OSM_Landmarks;
 
 namespace OSM_XML_Importer
 {
@@ -15,6 +14,110 @@ namespace OSM_XML_Importer
             IgnoreWhitespace = true,
             IgnoreComments = true
         };
+
+        public static Landmarks Import(string filePath = "", Logger?logger = null)
+        {
+            Dictionary<ulong, Node> nodes = new();
+
+            List<Address> addresses = new();
+            Address currentAddress;
+            Stream mapData = File.Exists(filePath) ? new FileStream(filePath, FileMode.Open, FileAccess.Read) : new MemoryStream(OSM_Data.map);
+
+            XmlReader reader = XmlReader.Create(mapData, readerSettings);
+            while (reader.ReadToFollowing("node"))
+            {
+                currentAddress = new Address();
+                XmlReader nodeReader = reader.ReadSubtree();
+                currentAddress.locationId = Convert.ToUInt64(reader.GetAttribute("id"));
+                currentAddress.lat = Convert.ToSingle(reader.GetAttribute("lat"));
+                currentAddress.lon = Convert.ToSingle(reader.GetAttribute("lon"));
+                nodes.Add(currentAddress.locationId, new Node(currentAddress.lat, currentAddress.lon));
+                while (nodeReader.ReadToDescendant("tag"))
+                {
+                    string key = (string)reader.GetAttribute("k");
+                    string value = (string)reader.GetAttribute("v");
+                    switch (key)
+                    {
+                        case "addr:street":
+                        case "addr:conscriptionnumber":
+                        case "addr:place":
+                            currentAddress.street = value;
+                            break;
+                        case "addr:housenumber":
+                        case "addr:housename":
+                        case "addr:flats":
+                            currentAddress.house = value;
+                            break;
+                        case "addr:postcode":
+                            currentAddress.zipCode = value;
+                            break;
+                        case "addr:city":
+                            currentAddress.city = value;
+                            break;
+                        case "addr:country":
+                            currentAddress.country = value;
+                            break;
+                    }
+                }
+                if (currentAddress.street != null)
+                {
+                    addresses.Add(currentAddress);
+                }
+            }
+
+            mapData.Position = 0;
+            reader = XmlReader.Create(mapData, readerSettings);
+            while (reader.ReadToFollowing("way"))
+            {
+                currentAddress = new Address();
+                XmlReader wayReader = reader.ReadSubtree();
+                while (wayReader.Read())
+                {
+                    if (wayReader.Name == "nd")
+                    {
+                        ulong id = Convert.ToUInt64(wayReader.GetAttribute("ref"));
+                        if (nodes.ContainsKey(id))
+                        {
+                            currentAddress.locationId = id;
+                            currentAddress.lat = nodes[id].lat;
+                            currentAddress.lon = nodes[id].lon;
+                        }
+                    }
+                    else if (wayReader.Name == "tag")
+                    {
+                        string key = (string)reader.GetAttribute("k");
+                        string value = (string)reader.GetAttribute("v");
+                        switch (key)
+                        {
+                            case "addr:street":
+                            case "addr:conscriptionnumber":
+                            case "addr:place":
+                                currentAddress.street = value;
+                                break;
+                            case "addr:housenumber":
+                            case "addr:housename":
+                            case "addr:flats":
+                                currentAddress.house = value;
+                                break;
+                            case "addr:postcode":
+                                currentAddress.zipCode = value;
+                                break;
+                            case "addr:city":
+                                currentAddress.city = value;
+                                break;
+                            case "addr:country":
+                                currentAddress.country = value;
+                                break;
+                        }
+                    }
+                }
+                if (currentAddress.street != null)
+                {
+                    addresses.Add(currentAddress);
+                }
+            }
+            return new Landmarks(addresses);
+        }
 
         public static Graph Import(string filePath = "", bool onlyJunctions = true, Logger? logger = null)
         {
